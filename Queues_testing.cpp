1d474bb8791b30d4269b7a4bfa40d0b6c857a69f
Vijay Chikkala <vijaykumar161206@gmail.com>
2025-07-18 09:29:59 +0530

    Add files via upload

---
+++ Queues_testing.cpp
@@ -0,0 +1,12 @@
+#include "Queues.h" 
+
+int main()
+{
+    enq(10);
+    enq(20);
+    enq(30);
+    enq(40);
+    display(); 
+    deq();     
+    display(); 
+}
---
+++ Stacks_testing.cpp
@@ -0,0 +1,12 @@
+#include "Stacks.h" 
+
+int main()
+{
+    push(10);
+    push(20);
+    push(30);
+    push(40);
+    display(); 
+    pop();     
+    display(); 
+}
